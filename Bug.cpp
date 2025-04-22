1f6e5fadfd8a7a9dd9b2dab7e370355fba41cc41
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-04-22 19:44:32 +0100

    created Bug class

---
+++ Bug.cpp
@@ -0,0 +1,205 @@
+#include "Bug.h"
+
+#include <random>
+#include <sstream>
+
+// allows non default values for Crawler
+Bug::Bug(unsigned int id, Position position, Direction direction, unsigned int size, bool alive, list<Position> path, int eatenByID)
+    : id(id), position(position), direction(direction), size(size), alive(alive), path(path), eatenByID(eatenByID)
+{
+}
+
+unsigned int Bug::getSize() const
+{
+    return size;
+}
+
+void Bug::setSize(unsigned int newSize)
+{
+    size = newSize;
+}
+
+bool Bug::isAlive() const
+{
+    return alive;
+}
+
+void Bug::setAlive(bool status)
+{
+    alive = status;
+}
+
+int Bug::getEatenByID() const
+{
+    return eatenByID;
+}
+
+void Bug::setEatenByID(int eaterID)
+{
+    eatenByID = eaterID;
+}
+
+string Bug::getBugDetails() const
+{
+    return "Bug ID: " + to_string(id) 
+        + ", Position(x, y): (" + to_string(position.x) + ", " + to_string(position.y) + "), " 
+        + "Size: " + to_string(size) 
+        + ", Direction: " + getDirectionString() 
+        + ", Status: " + (alive ? "Alive" : "Dead");
+}
+
+string Bug::getDirectionString() const
+{
+    switch (direction)
+    {
+    case Direction::North:
+        return "North";
+    case Direction::East:
+        return "East";
+    case Direction::South:
+        return "South";
+    case Direction::West:
+        return "West";
+    default:
+        return "Unknown";
+    }
+}
+
+unsigned int Bug::getBugID() const
+{
+    return id;
+}
+
+Position Bug::getPosition() const
+{
+    return position;
+}
+
+bool Bug::isWayBlocked(Position newP)
+{
+    return !(newP.x >= 0 && newP.x < 10 && newP.y >= 0 && newP.y < 10);
+}
+
+// void Bug::move()
+// {
+//     bool moved = false;
+//     while (!moved)
+//     {
+//         Position newP = position;
+//         switch (direction)
+//         {
+//         case Direction::North:
+//             newP.y -= 1;
+//             break;
+//         case Direction::East:
+//             newP.x += 1;
+//             break;
+//         case Direction::South:
+//             newP.y += 1;
+//             break;
+//         case Direction::West:
+//             newP.x -= 1;
+//             break;
+//         default:
+//             break;
+//         }
+//
+//         if (isWayBlocked(newP))
+//         {
+//             srand(time(0));
+//             // generate between 1 and 4
+//             int newDirection = 1 + rand() % 4;
+//             // new direction for crawler
+//             direction = static_cast<Direction>(newDirection);
+//         }
+//         else
+//         {
+//             // update new position
+//             position = newP;
+//             // added the new position to path list
+//             path.push_back(position);
+//             moved = true;
+//         }
+//     }
+// }
+
+void Bug::fight(vector<Bug*>& bugsInCell)
+{
+    if (bugsInCell.size() < 2) return; // no fight happens if bugs < 2
+
+    // sort bugs by size in descending order
+    sort(bugsInCell.begin(), bugsInCell.end(),
+        [](Bug* a, Bug* b) { return a->getSize() > b->getSize(); });
+
+    // find the largest size
+    unsigned int maxSize = bugsInCell[0]->getSize();
+    vector<Bug*> biggestBugs;
+
+    // find all bugs with the largest size
+    for (Bug* bug : bugsInCell)
+    {
+        if (bug->getSize() == maxSize)
+        {
+            biggestBugs.push_back(bug);
+        }
+        else
+        {
+            break; // stop the loop when it reaches smaller bugs (works because the vector is sorted)
+        }
+    }
+
+    // determine the winner
+    Bug* winner;
+    if (biggestBugs.size() > 1) // if multiple bugs have the max size, pick one randomly
+    {
+        srand(time(NULL));
+        int randomIndex = rand() % biggestBugs.size();
+        winner = biggestBugs[randomIndex];
+    }
+    else
+    {
+        winner = biggestBugs[0];
+    }
+
+    // display fight results
+    cout << "Crawler " << winner->getBugID() << " wins the fight at ("
+         << winner->getPosition().x << ", " << winner->getPosition().y << ")\n";
+
+    // winner eats all other bugs
+    for (Bug* bug : bugsInCell) {
+        if (bug != winner && bug->isAlive()) {
+            cout << "Crawler " << bug->getBugID() << " is eaten by Crawler " << winner->getBugID() << "\n\n";
+            winner->setSize(winner->getSize() + bug->getSize()); // increase winner's size by loser's size
+            bug->setAlive(false); // mark the eaten bug as dead
+            bug->setEatenByID(winner->getBugID()); // record who ate the losing bug
+        }
+    }
+}
+
+string Bug::getLifeHistory() const
+{
+    stringstream history;
+    history << id << " Crawler Path: ";
+
+    if (path.empty())
+    {
+        history << "No movement recorded";
+    }
+    else
+    {
+        for (auto it = path.begin(); it != path.end(); ++it) {
+            history << "(" << it->x << "," << it->y << ")";
+            if (next(it) != path.end()) history << " -> ";
+        }
+    }
+
+    if (isAlive())
+    {
+        history << " Alive";
+    }
+    else
+    {
+        history << " Eaten by " << to_string(getEatenByID());
+    }
+    return history.str();
+}
---
+++ Bug.h
@@ -0,0 +1,54 @@
+#ifndef BUG_H
+#define BUG_H
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+struct Position
+{
+    int x;
+    int y;
+};
+
+enum class Direction
+{
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4
+};
+
+
+class Bug
+{
+protected: // Changed from private to protected so derived classes can access
+    unsigned int id;
+    Position position;
+    Direction direction;
+    unsigned int size;
+    bool alive;
+    list<Position> path;
+    int eatenByID;
+
+public:
+    Bug(unsigned int id, Position position, Direction direction, unsigned int size, bool alive = true, list<Position> path = {}, int eatenByID = -1);
+    unsigned int getSize() const;
+    void setSize(unsigned int newSize);
+    bool isAlive() const;
+    void setAlive(bool status);
+    int getEatenByID() const;
+    void setEatenByID(int eaterID);
+    bool isWayBlocked(Position newP);
+    string getBugDetails() const;
+    string getDirectionString() const;
+    unsigned int getBugID() const;
+    Position getPosition() const;
+    string getLifeHistory() const;
+    void fight(vector<Bug*>& bugsInCell);
+    virtual void move() = 0;
+};
+
+
+#endif //BUG_H
